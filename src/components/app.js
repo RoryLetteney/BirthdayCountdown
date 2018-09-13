ae6960f3f1dbdd7909203058e149c590f0733e6d
Rory Letteney <rory.letteney@gmail.com>
2018-09-13 17:42:11 -0500

    Installed moment.js, react-datepicker, and placed picker component on grid with datepicker within

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Picker from "./picker";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='grid'>
+      <div className="grid">
         <h2 className="grid__title">Birthday Countdown</h2>
 
-        <div className="grid__skew-dark-one-box"></div>
-        <div className="grid__skew-dark-two"></div>
+        <div className="grid__skew-dark-one-box" />
+        <div className="grid__skew-dark-two" />
 
-        <div className="grid__skew-light-one"></div>
-        <div className="grid__skew-light-two"></div>
-        <div className="grid__skew-light-three-box"></div>
+        <div className="grid__skew-light-one" />
+        <div className="grid__skew-light-two" />
+        <div className="grid__skew-light-three-box" />
+
+        <Picker />
       </div>
     );
   }
---
+++ src/components/picker.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import DatePicker from "react-datepicker";
+import moment from "moment";
+
+export default class Picker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: moment()
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(date) {
+    console.log("trying to change date to", date._d);
+    this.setState({ startDate: date });
+  }
+
+  render() {
+    return (
+      <div className="picker">
+        <DatePicker
+          selected={this.state.startDate}
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
+}
