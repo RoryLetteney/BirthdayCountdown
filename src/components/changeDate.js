4094bdd87d541a13bc92435fdc0692c429840f25
Rory Letteney <rory.letteney@gmail.com>
2018-09-14 07:54:46 -0500

    Added change date component and functionality

---
+++ src/components/changeDate.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const ChangeDate = (title, callback) => {
+   return (
+       <button className="change-date" onClick={callback}>
+           <i className="far fa-calendar-alt"></i>
+           {title}
+       </button>
+   )
+}
