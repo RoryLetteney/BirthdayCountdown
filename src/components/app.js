a760b24d001fb16f01b8645fef00e578f4d8df98
Rory Letteney <rory.letteney@gmail.com>
2018-09-13 19:53:24 -0500

    Built and styled clock component

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Picker from "./picker";
 import { Button } from "./button";
+import Clock from "./clock";
 
 export default class App extends Component {
   render() {
@@ -16,7 +17,8 @@ export default class App extends Component {
         <div className="grid__skew-light-three-box" />
 
         <Picker />
-        {Button('Generate Countdown')}
+        {Button("Generate Countdown")}
+        <Clock />
       </div>
     );
   }
